ced6e1aeb1f5222cefef59eaa1dc98a6dad9dadd
JP <85654561+jp524@users.noreply.github.com>
2022-08-30 16:37:58 -0400

    Build #build_tree

--- knight.rb
+++ knight.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: false
 
+# Creates a node containing data and its children
+class Node
+  attr_accessor :data, :children
+
+  def initialize(data)
+    @data = data
+    @children = []
+  end
+end
+
+# Generates the availables moves from a starting position
 class Knight
+  attr_reader :root
+
+  def initialize
+    @root = []
+  end
+
   def knight_moves(start, finish)
-    puts 'move' if valid_move?(start, finish)
+    @root = Node.new(start) if valid_move?(start, finish)
+    build_tree
   end
 
   def valid_move?(start, finish)
@@ -28,9 +46,22 @@ class Knight
              [x + 2, y + 1],
              [x + 2, y - 1]]
     moves.filter! { |move| (0..7).include?(move[0]) && (0..7).include?(move[1]) }
+    moves
+  end
+
+  def build_tree(root = @root, counter = 0)
+    return if counter == 3
+
+    counter += 1
+    children = available_moves(root.data)
+    children.each do |child|
+      node = Node.new(child)
+      root.children << node
+      build_tree(node, counter)
+    end
   end
 end
 
 game = Knight.new
-# game.knight_moves([0, 8], [0, 0])
-p game.available_moves([0, 0])
+game.knight_moves([0, 0], [3, 1])
+p game.root
